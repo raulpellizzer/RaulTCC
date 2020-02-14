ca4bbc50dedb610d8fdf7ce97e3fa47317e18bcc
Pellizzer,Raul <raul1.pellizzer1@gmail.com>
2020-02-14 19:26:52 -0300

    Setting up environment

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-const {Builder, By, Key, util, until} = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+class TCC {
 
-const driver = new Builder().forBrowser("chrome").build();
+    constructor() {
+        const fs = require('fs');
 
-driver.get("https://google.com.br");
-driver.manage().window().maximize();    
-driver.findElement(By.name("q")).sendKeys("Americanas.com.br", Key.ENTER);
+        const {Builder, By, Key, util, until} = require("selenium-webdriver");
+        const chrome = require("selenium-webdriver/chrome");
+        const driver = new Builder().forBrowser("chrome").build();
+    }
 
-let initialLink = driver.wait(until.elementLocated(By.className('LC20lb'), 10000));
-initialLink.click();
+    AmericanasHandler(driver)
+    {
+        driver.get("https://google.com.br");
+        driver.manage().window().maximize();    
+        driver.findElement(By.name("q")).sendKeys("Americanas.com.br", Key.ENTER);
 
-let americanasSearchBar = driver.wait(until.elementLocated(By.id('h_search-input'), 10000));
-americanasSearchBar.sendKeys("Headphones", Key.ENTER);
+        let initialLink = driver.wait(until.elementLocated(By.className('LC20lb'), 10000));
+        initialLink.click();
 
-setTimeout(() => {
-    console.log("Continuing ..");
-}, 45000);
+        let americanasSearchBar = driver.wait(until.elementLocated(By.id('h_search-input'), 10000));
+        americanasSearchBar.sendKeys("Headphones", Key.ENTER);
 
+        setTimeout(() => {
+            console.log("Continuing ..");
+        }, 45000);
+    }
 
+    PrintLine() 
+    {
+        console.log('Reading file ..');
+    }
+
+    ReadFile(filePath, encod)
+    {
+        fs.readFile(filePath, encod, (err, data) => {
+            if (err) throw err
+            console.log(data);
+        });
+    }
+}
