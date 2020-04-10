5afe9725bb90d89439fc963648d9b7b4591a3083
Pellizzer,Raul <raul1.pellizzer1@gmail.com>
2020-04-10 15:26:10 -0300

    TCC:WIP - Getting data from BuscaPe

--- src/index.js
+++ src/index.js
@@ -24,48 +24,12 @@ class Main {
      * 
      * @returns
      */
-    async StartExecution() {
+    async MainExecution() {
         try {
-            this.report.SetReportPath(__dirname + "\\data\\Search.txt");
-            this.reportPath = this.report.GetReportPath();
-
-            this.buscaPe.ChromeDriverStartUp(); 
-            await this.buscaPe.NavigateToBuscaPe();
-            await this.buscaPe.MaximizeWindow();
-            await this.buscaPe.ScrollToBottom();
-            this.searchTags = await this.iniHandler.GetIniConfig();
-
-            await this.buscaPe.QueryItem(this.searchTags);
-            this.products = await this.buscaPe.GetProducts();
-
-            for (let index = 0; index < this.products.length; index++) {
-                await this.buscaPe.DriverSleep(200);
-                this.products = await this.buscaPe.GetProducts();
-                let element = this.products[index];
-
-                if (await element.getText() ==  "Ver preços") {
-                    await element.click();
-
-                    let productData = await this.buscaPe.GetProductData(false, index);
-                    this.buscaPeData.push(productData);
-
-                    await this.buscaPe.NavigateToPreviousPage();
-                    await this.buscaPe.DriverSleep(100);
-                } else {
-                    let productData = await this.buscaPe.GetProductData(true, index);
-                    this.buscaPeData.push(productData);
-                }
-            }
-
-            for (let index = 0; index < this.buscaPeData.length; index++) { 
-                this.report.RegisterDataInFile(this.reportPath, this.buscaPeData[index].productName + "\n");
-                for (let innerIndex = 0; innerIndex < this.buscaPeData[index].productPrices.length; innerIndex++) {
-                    if (this.buscaPeData[index].productPrices[innerIndex] == undefined)
-                        continue;
-                    this.report.RegisterDataInFile(this.reportPath, this.buscaPeData[index].productPrices[innerIndex] + "\n");
-                }
-                this.report.RegisterDataInFile(this.reportPath, "\n\n");
-            }
+            await this.Initialize();
+            await this.QueryAndGetProducts();
+            await this.RetrieveData();
+            await this.GenerateTXTReport();
 
             console.log("Done!");
 
@@ -74,8 +38,76 @@ class Main {
         }
     }
 
+    /**
+     * Starts browser and gets configuration tags
+     * 
+     * @returns
+     */
+    async Initialize() {
+        this.report.SetReportPath(__dirname + "\\data\\Search.txt");
+        this.reportPath = this.report.GetReportPath();
+
+        this.buscaPe.ChromeDriverStartUp(); 
+        await this.buscaPe.NavigateToBuscaPe();
+        await this.buscaPe.MaximizeWindow();
+        await this.buscaPe.ScrollToBottom();
+        this.searchTags = await this.iniHandler.GetIniConfig();
+    }
+
+    /**
+     * Queries the product in BuscaPe and retrieve main data
+     * 
+     * @returns
+     */
+    async QueryAndGetProducts() {
+        this.buscaPe.QueryItem(this.searchTags);
+        this.products = await this.buscaPe.GetProducts();
+    }
+
+    /**
+     * Retrieve data about all products
+     * 
+     * @returns
+     */
+    async RetrieveData() {
+        for (let index = 0; index < this.products.length; index++) {
+            await this.buscaPe.DriverSleep(200);
+            this.products = await this.buscaPe.GetProducts();
+            let element = this.products[index];
+
+            if (await element.getText() ==  "Ver preços") {
+                await element.click();
+
+                let productData = await this.buscaPe.GetProductData(false, index);
+                this.buscaPeData.push(productData);
+
+                await this.buscaPe.NavigateToPreviousPage();
+                await this.buscaPe.DriverSleep(100);
+            } else {
+                let productData = await this.buscaPe.GetProductData(true, in6dex);
+                this.buscaPeData.push(productData);
+            }
+        }
+    }
+
+    /**
+     * Generates a .txt report file
+     * 
+     * @returns
+     */
+    async GenerateTXTReport() {
+        for (let index = 0; index < this.buscaPeData.length; index++) {
+            this.report.RegisterDataInFile(this.reportPath, this.buscaPeData[index].productName + "\n");
+            for (let innerIndex = 0; innerIndex < this.buscaPeData[index].productPrices.length; innerIndex++) {
+                if (this.buscaPeData[index].productPrices[innerIndex] == undefined)
+                    continue;
+                this.report.RegisterDataInFile(this.reportPath, this.buscaPeData[index].productPrices[innerIndex] + "\n");
+            }
+            this.report.RegisterDataInFile(this.reportPath, "\n\n");
+        }
+    }
 }
 
 let main = new Main();
-main.StartExecution();
+main.MainExecution();
 
